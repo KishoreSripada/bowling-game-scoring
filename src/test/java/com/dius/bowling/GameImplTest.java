4a9dc568d3d80ee783f4a3d0678f3a24bb3d1033
Kishore Sripada <kishore.sripada@sensis.com.au>
2019-11-07 17:59:35 +1100

    Added test for scenarios that are neither spare nor strike:

--- src/test/java/com/dius/bowling/GameImplTest.java
+++ src/test/java/com/dius/bowling/GameImplTest.java
@@ -18,4 +18,18 @@ public class GameImplTest {
         game.roll(0);
         assertEquals(0, game.score());
     }
+
+    @Test
+    public void test_when_roll_is_1_and_0_to_score_1() throws Exception {
+        game.roll(1);
+        game.roll(0);
+        assertEquals(1, game.score());
+    }
+
+    @Test
+    public void test_when_roll_is_4_and_4_to_score_8() throws Exception {
+        game.roll(4);
+        game.roll(4);
+        assertEquals(8, game.score());
+    }
 }
